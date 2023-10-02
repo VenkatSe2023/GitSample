5f8f4696b6b9e508d7ef950095bc6e9be5915f24
Venkat <venkat.selenium2011@gmai.com>
2023-10-02 16:21:56 +0530

    from USer

--- src/org/Sample.java
+++ src/org/Sample.java
@@ -4,10 +4,19 @@ public class Sample {
 
 	public static void main(String[] args) {
 		System.out.println("Git - 1");
+		
+		Sample obj = new Sample();
+		obj.method1();
+		obj.method2();
+		
 	}
 	
 	public void method1() {
 		System.out.println("method1 added");
+	}
+	
+	public void method2() {
+		System.out.println("method2 added");
 	}
 
 }
